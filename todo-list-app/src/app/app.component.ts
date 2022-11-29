89ecace279d5997e5baa89a8ec1179b8e0387014
Luís Fernando Teixeira Bicalho <lfernandotexbicalho@hotmail.com>
2022-11-29 11:15:53 -0300

    Added the structure for the todo-list-app

---
+++ todo-list-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template: `
+  //   <h1>Bem-vindo ao App de Listagem de Tarefas</h1>
+  // `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
---
+++ todo-list-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+import { TaskListComponent } from './components/task-list/task-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
